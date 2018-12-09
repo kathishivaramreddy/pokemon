f7db558994abc252eef6035147876f61cd787871
kathishivaramreddy <kathishivaram@gmail.com>
2018-12-10 00:44:22 +0530

    Adding modal to show individual pokemon details

--- src/Pokemons.js
+++ src/Pokemons.js
@@ -3,18 +3,34 @@ import './Pokemons.css';
 import {ImageSlider} from './ImageSlider'
 import {fetchPokemonDetails} from './actions/pokemonDetails'
 import {connect} from 'react-redux';
+import Modal from 'react-responsive-modal';
 
 export class Pokemons extends React.Component{
 
     constructor(props){
         super(props);
+        this.state={open:false}
     }
 
+    onOpenModal = () => {
+        this.setState({ open: true });
+    };
+
+    onCloseModal = () => {
+        this.setState({ open: false });
+    };
+
+
     render(){
+        const { open } = this.state;
         return(
             this.props.pokedata.pokedata.map( (pokemon,index) => <div className="pokemons" >
                 <ImageSlider index={index+1}/>
-                <strong onClick={() => this.props.fetchPokemonDetails(pokemon.url)}>{pokemon.name}</strong>
+                <strong onClick={() => this.props.fetchPokemonDetails(pokemon.url), this.onOpenModal }>{pokemon.name}</strong>
+                <Modal open={open} onClose={this.onCloseModal}
+                       abilities={this.props.pokedetails.abilities}>
+                    <h2>Simple centered modal</h2>
+                </Modal>
                 </div>
             )
 
